4107535a106183d35941e314f310b24383e6c466
Milko Daskalov <mdaskalov@outlook.com>
2020-09-26 02:25:07 +0200

    update configuration

--- src/platform.ts
+++ src/platform.ts
@@ -74,8 +74,8 @@ export class ZbBridgePlatform implements DynamicPlatformPlugin {
         this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
 
         // if you need to update the accessory.context then you should run `api.updatePlatformAccessories`. eg.:
-        // existingAccessory.context.device = device;
-        // this.api.updatePlatformAccessories([existingAccessory]);
+        existingAccessory.context.device = device;
+        this.api.updatePlatformAccessories([existingAccessory]);
 
         // create the accessory handler for the restored accessory
         // this is imported from `platformAccessory.ts`
