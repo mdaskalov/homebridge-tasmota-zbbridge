d8db533b0b23cce32f8202022a7ad35457d235ba
Milko Daskalov <mdaskalov@outlook.com>
2020-09-27 20:55:19 +0200

    check if there are configured devices

--- src/platform.ts
+++ src/platform.ts
@@ -5,17 +5,12 @@ import { ZbBridgeAccessory } from './zbBridgeAccessory';
 import { TasmotaAccessory } from './tasmotaAccessory';
 import { MQTTClient } from './mqttClient';
 
-/**
- * HomebridgePlatform
- * This class is the main constructor for your plugin, this is where you should
- * parse the user config and discover/register accessories with Homebridge.
- */
 export class TasmotaZbBridgePlatform implements DynamicPlatformPlugin {
   public readonly Service: typeof Service = this.api.hap.Service;
   public readonly Characteristic: typeof Characteristic = this.api.hap.Characteristic;
   public readonly mqttClient = new MQTTClient(this.log, this.config);
 
-  // this is used to track restored cached accessories
+  // cached accessories
   public readonly accessories: PlatformAccessory[] = [];
 
   constructor(
@@ -25,102 +20,54 @@ export class TasmotaZbBridgePlatform implements DynamicPlatformPlugin {
   ) {
     this.log.debug('Finished initializing platform:', this.config.name);
 
-    // When this event is fired it means Homebridge has restored all cached accessories from disk.
-    // Dynamic Platform plugins should only register new accessories after this event was fired,
-    // in order to ensure they weren't added to homebridge already. This event can also be used
-    // to start discovery of new accessories.
     this.api.on('didFinishLaunching', () => {
       log.debug('Executed didFinishLaunching callback');
-      // run the method to discover / register your devices as accessories
       this.discoverDevices();
     });
   }
 
-  /**
-   * This function is invoked when homebridge restores cached accessories from disk at startup.
-   * It should be used to setup event handlers for characteristics and update respective values.
-   */
   configureAccessory(accessory: PlatformAccessory) {
     this.log.info('Loading accessory from cache:', accessory.displayName);
-
-
-    // add the restored accessory to the accessories cache so we can track if it has already been registered
     this.accessories.push(accessory);
   }
 
-  /**
-   * This is an example method showing how to register discovered accessories.
-   * Accessories must only be registered once, previously created accessories
-   * must not be registered again to prevent "duplicate UUID" errors.
-   */
   discoverDevices() {
-    // this.mqttClient.subscribe('stat/zbbridge/RESULT', msg => {
-    //   this.log.info('Result', msg);
-    // });
-
-    // loop over the discovered devices and register each one if it has not already been registered
-    for (const device of this.config.zbBridgeDevices) {
-
-      // generate a unique id for the accessory this should be generated from
-      // something globally unique, but constant, for example, the device serial
-      // number or MAC address
-      const uuid = this.api.hap.uuid.generate(device.addr);
-
-      // see if an accessory with the same uuid has already been registered and restored from
-      // the cached devices we stored in the `configureAccessory` method above
-      const existingAccessory = this.accessories.find(accessory => accessory.UUID === uuid);
-
-      if (existingAccessory) {
-        // the accessory already exists
-        this.log.info('Restoring existing zbBridge accessory from cache:', existingAccessory.displayName);
-
-        // if you need to update the accessory.context then you should run `api.updatePlatformAccessories`. eg.:
-        existingAccessory.context.device = device;
-        this.api.updatePlatformAccessories([existingAccessory]);
-
-        // create the accessory handler for the restored accessory
-        // this is imported from `platformAccessory.ts`
-        new ZbBridgeAccessory(this, existingAccessory);
-
-      } else {
-        // the accessory does not yet exist, so we need to create it
-        this.log.info('Adding new zbBridge accessory:', device.name);
-
-        // create a new accessory
-        const accessory = new this.api.platformAccessory(device.name, uuid);
-
-        // store a copy of the device object in the `accessory.context`
-        // the `context` property can be used to store any data about the accessory you may need
-        accessory.context.device = device;
-
-        // create the accessory handler for the newly create accessory
-        // this is imported from `platformAccessory.ts`
-        new ZbBridgeAccessory(this, accessory);
-
-        // link the accessory to your platform
-        this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
+    if (Array.isArray(this.config.zbBridgeDevices)) {
+      for (const device of this.config.zbBridgeDevices) {
+        const uuid = this.api.hap.uuid.generate(device.addr);
+        const existingAccessory = this.accessories.find(accessory => accessory.UUID === uuid);
+        if (existingAccessory) {
+          this.log.info('Restoring existing zbBridge accessory from cache:', existingAccessory.displayName);
+          existingAccessory.context.device = device;
+          this.api.updatePlatformAccessories([existingAccessory]);
+          new ZbBridgeAccessory(this, existingAccessory);
+        } else {
+          this.log.info('Adding new zbBridge accessory:', device.name);
+          const accessory = new this.api.platformAccessory(device.name, uuid);
+          accessory.context.device = device;
+          new ZbBridgeAccessory(this, accessory);
+          this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
+        }
       }
-
-      // it is possible to remove platform accessories at any time using `api.unregisterPlatformAccessories`, eg.:
-      // this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
     }
-
-    for (const device of this.config.tasmotaDevices) {
-      const uuid = this.api.hap.uuid.generate(device.topic + '-' + device.type);
-      const existingAccessory = this.accessories.find(accessory => accessory.UUID === uuid);
-      if (existingAccessory) {
-        this.log.info('Restoring existing tasmota accessory from cache:', existingAccessory.displayName);
-        existingAccessory.context.device = device;
-        this.api.updatePlatformAccessories([existingAccessory]);
-        new TasmotaAccessory(this, existingAccessory);
-
-      } else {
-        this.log.info('Adding new tasmota accessory:', device.name);
-        const accessory = new this.api.platformAccessory(device.name, uuid);
-        accessory.context.device = device;
-        new TasmotaAccessory(this, accessory);
-        this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
+    if (Array.isArray(this.config.tasmotaDevices)) {
+      for (const device of this.config.tasmotaDevices) {
+        const uuid = this.api.hap.uuid.generate(device.topic + '-' + device.type);
+        const existingAccessory = this.accessories.find(accessory => accessory.UUID === uuid);
+        if (existingAccessory) {
+          this.log.info('Restoring existing tasmota accessory from cache:', existingAccessory.displayName);
+          existingAccessory.context.device = device;
+          this.api.updatePlatformAccessories([existingAccessory]);
+          new TasmotaAccessory(this, existingAccessory);
+        } else {
+          this.log.info('Adding new tasmota accessory:', device.name);
+          const accessory = new this.api.platformAccessory(device.name, uuid);
+          accessory.context.device = device;
+          new TasmotaAccessory(this, accessory);
+          this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
+        }
       }
     }
   }
+
 }
