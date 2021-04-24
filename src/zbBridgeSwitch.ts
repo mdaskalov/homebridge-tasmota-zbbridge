e1c1852ffca30d2dfc53263884e7b0dd08a653a3
Milko Daskalov <mdaskalov@outlook.com>
2021-04-24 09:37:33 +0200

    implemented abstract methods

--- src/zbBridgeSwitch.ts
+++ src/zbBridgeSwitch.ts
@@ -1,28 +1,55 @@
 import {
+  Service,
   PlatformAccessory,
   CharacteristicValue,
-  Service,
 } from 'homebridge';
 
 import { ZbBridgeAccessory } from './zbBridgeAccessory';
 import { TasmotaZbBridgePlatform } from './platform';
 
 export class ZbBridgeSwitch extends ZbBridgeAccessory {
-  private service: Service;
   private power?: CharacteristicValue;
+
   constructor(platform: TasmotaZbBridgePlatform, accessory: PlatformAccessory) {
     super(platform, accessory);
-
-    // get the service if it exists, otherwise create a new service
-    const service = this.type === 'switch' ? this.platform.Service.Switch : this.platform.Service.Lightbulb;
-    this.service = this.accessory.getService(service) || this.accessory.addService(service);
-
-    // set the service name, this is what is displayed as the default name on the Home app
-    this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.name);
-
   }
 
-  // getService() : Service {
-  //   return Service.Switch;
-  // }
+  getService(): Service {
+    const service = this.platform.Service.Switch;
+    // get the service if it exists, otherwise create a new service
+    return this.accessory.getService(service) || this.accessory.addService(service);
+  }
+
+  registerHandlers() {
+    // register handlers for the On/Off Characteristic
+    this.service.getCharacteristic(this.platform.Characteristic.On)
+      .onSet(this.setOn.bind(this))
+      .onGet(this.getOn.bind(this));
+  }
+
+  onStatusUpdate(response) {
+    if (response.Power !== undefined) {
+      this.power = (response.Power === 1);
+      this.service.getCharacteristic(this.platform.Characteristic.On).updateValue(this.power);
+    }
+    this.platform.log.debug('%s (%s) %s',
+      this.accessory.context.device.name, this.addr,
+      this.power !== undefined ? 'Power: ' + (this.power ? 'On' : 'Off') : '',
+    );
+  }
+
+  async setOn(value: CharacteristicValue) {
+    if (this.power !== value) {
+      this.power = value as boolean;
+      this.platform.mqttClient.send({ device: this.addr, send: { Power: (this.power ? 'On' : 'Off') } });
+    }
+  }
+
+  async getOn(): Promise<CharacteristicValue> {
+    if (this.power === undefined) {
+      this.platform.mqttClient.send({ device: this.addr, cluster: 6, read: 0 });
+      throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
+    }
+    return this.power;
+  }
 }
