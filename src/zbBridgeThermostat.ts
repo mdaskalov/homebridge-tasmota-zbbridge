e61d207b2c972a0898930574e6376d3df8713f90
Milko Daskalov <mdaskalov@outlook.com>
2022-10-02 17:33:38 +0200

    use zbbridge characteristic

--- src/zbBridgeThermostat.ts
+++ src/zbBridgeThermostat.ts
@@ -1,24 +1,91 @@
 import {
-  PlatformAccessory,
   CharacteristicValue,
-  HAPStatus,
+  PlatformAccessory,
 } from 'homebridge';
 
 import { ZbBridgeAccessory } from './zbBridgeAccessory';
 import { TasmotaZbBridgePlatform } from './platform';
-import { ZbBridgeValue } from './zbBridgeValue';
+import { ZbBridgeCharacteristic } from './zbBridgeCharacteristic';
 
 export class ZbBridgeThermostat extends ZbBridgeAccessory {
-  private currentTemperature: ZbBridgeValue;
-  private targetTemperature: ZbBridgeValue;
+  private currentTemperature: ZbBridgeCharacteristic;
+  private targetTemperature: ZbBridgeCharacteristic;
+
+  private currentHeatingCoolingState: ZbBridgeCharacteristic;
+  private targetHeatingCoolingState: ZbBridgeCharacteristic;
+  private heatingThresholdTemperature: ZbBridgeCharacteristic;
+  private coolingThresholdTemperature: ZbBridgeCharacteristic;
 
   constructor(
     readonly platform: TasmotaZbBridgePlatform,
     readonly accessory: PlatformAccessory,
   ) {
     super(platform, accessory);
-    this.currentTemperature = new ZbBridgeValue(platform, accessory, 'currentTemperature', 20);
-    this.targetTemperature = new ZbBridgeValue(platform, accessory, 'targetTemperature', 22);
+
+    this.currentTemperature = new ZbBridgeCharacteristic(this.platform, this.accessory, this.service,
+      'CurrentTemperature', 20);
+
+    this.targetTemperature = new ZbBridgeCharacteristic(this.platform, this.accessory, this.service,
+      'TargetTemperature', 22);
+
+    this.currentHeatingCoolingState = new ZbBridgeCharacteristic(this.platform, this.accessory, this.service,
+      'CurrentHeatingCoolingState', this.platform.Characteristic.CurrentHeatingCoolingState.OFF);
+
+    this.targetHeatingCoolingState = new ZbBridgeCharacteristic(this.platform, this.accessory, this.service,
+      'TargetHeatingCoolingState', this.platform.Characteristic.TargetHeatingCoolingState.AUTO);
+
+    this.heatingThresholdTemperature = new ZbBridgeCharacteristic(this.platform, this.accessory, this.service,
+      'HeatingThresholdTemperature', 20);
+
+    this.coolingThresholdTemperature = new ZbBridgeCharacteristic(this.platform, this.accessory, this.service,
+      'CoolingThresholdTemperature', 30);
+
+    this.currentTemperature.willGet = (value, needsUpdate) => {
+      this.log('willGet currentTemperature to: %s, needsUpdate %s', value, needsUpdate);
+      if (needsUpdate) {
+        this.zbInfo();
+        return undefined;
+      }
+      return value;
+    };
+    this.targetTemperature.willGet = (value, needsUpdate) => {
+      this.log('willGet targetTemperature to: %s, needsUpdate %s', value, needsUpdate);
+      if (needsUpdate) {
+        this.zbInfo();
+        return undefined;
+      }
+      return value;
+    };
+
+    this.heatingThresholdTemperature.willGet = (value, needsUpdate) => {
+      this.log('willGet heatingThresholdTemperature to: %s, needsUpdate %s', value, needsUpdate);
+      if (needsUpdate) {
+        this.zbInfo();
+        return undefined;
+      }
+      return value;
+    };
+
+    this.coolingThresholdTemperature.willGet = (value, needsUpdate) => {
+      this.log('willGet coolingThresholdTemperature to: %s, needsUpdate %s', value, needsUpdate);
+      if (needsUpdate) {
+        this.zbInfo();
+        return undefined;
+      }
+      return value;
+    };
+
+    this.targetTemperature.willSet = (value: CharacteristicValue) => {
+      this.log('willSet targetTemperature to:', value);
+    };
+
+    this.heatingThresholdTemperature.willSet = (value: CharacteristicValue) => {
+      this.log('willSet heatingThresholdTemperature to:', value);
+    };
+
+    this.coolingThresholdTemperature.willSet = (value: CharacteristicValue) => {
+      this.log('willSet coolingThresholdTemperature to:', value);
+    };
   }
 
   getServiceName() {
@@ -26,68 +93,33 @@ export class ZbBridgeThermostat extends ZbBridgeAccessory {
   }
 
   registerHandlers() {
-    this.service.getCharacteristic(this.platform.Characteristic.CurrentTemperature)
-      .onGet(this.getCurrentTemperature.bind(this));
-    this.service.getCharacteristic(this.platform.Characteristic.TargetTemperature)
-      .onSet(this.setTargetTemperature.bind(this))
-      .onGet(this.getTargetTemperature.bind(this));
-  }
-
-  updateCurrentTemperature(msg) {
-    let statusText = '';
-    if (msg.LocalTemperature !== undefined) {
-      const localTemperature = msg.LocalTemperature;
-      const ignoreCurrentTemperature = this.currentTemperature.update(localTemperature);
-      if (!ignoreCurrentTemperature) {
-        this.service.getCharacteristic(this.platform.Characteristic.CurrentTemperature).updateValue(localTemperature);
-        statusText += ` CurrentTemperature: ${localTemperature}`;
-      }
-    }
-    return statusText;
-  }
-
-  updateTargetTemperature(msg) {
-    let statusText = '';
-    if (msg.TuyaTempTarget !== undefined) {
-      const tempTarget = msg.TuyaTempTarget;
-      const ignoreTempTarget = this.targetTemperature.update(tempTarget);
-      if (!ignoreTempTarget) {
-        this.service.getCharacteristic(this.platform.Characteristic.TargetTemperature).updateValue(tempTarget);
-        statusText += ` TargetTemperature: ${tempTarget}`;
-      }
-    }
-    return statusText;
+    return;
   }
 
   onStatusUpdate(msg): string {
     let statusText = '';
-    statusText += this.updateTargetTemperature(msg);
-    statusText += this.updateCurrentTemperature(msg);
+
+    if (msg.Temperature !== undefined) {
+      statusText += this.currentTemperature.update(msg.Temperature);
+    }
+    if (msg.LocalTemperature !== undefined) { // Tuya
+      statusText += this.currentTemperature.update(msg.LocalTemperature);
+    }
+    if (msg.TempTarget !== undefined) {
+      statusText += this.targetTemperature.update(msg.TempTarget);
+    }
+    if (msg.TuyaTempTarget !== undefined) {
+      statusText += this.targetTemperature.update(msg.TuyaTempTarget);
+    }
+
+    if (msg.TuyaEcoTemp !== undefined) {
+      statusText += this.heatingThresholdTemperature.update(msg.TuyaEcoTemp);
+    }
+    if (msg.TuyaComfortTemp !== undefined) {
+      statusText += this.coolingThresholdTemperature.update(msg.TuyaComfortTemp);
+    }
+
     return statusText;
-  }
-
-  async getCurrentTemperature(): Promise<CharacteristicValue> {
-    const currentTemperature = this.currentTemperature.get();
-    if (this.currentTemperature.needsUpdate()) {
-      this.zbInfo();
-      throw new this.platform.api.hap.HapStatusError(HAPStatus.OPERATION_TIMED_OUT);
-    }
-    return currentTemperature;
-  }
-
-  async setTargetTemperature(value: CharacteristicValue) {
-    const targetTemperature = value as number;
-    this.targetTemperature.set(targetTemperature);
-    //this.zbSend({ Device: this.addr, Endpoint: this.endpoint, Send: { ? } });
-  }
-
-  async getTargetTemperature(): Promise<CharacteristicValue> {
-    const targetTemperature = this.targetTemperature.get();
-    if (this.targetTemperature.needsUpdate()) {
-      this.zbInfo();
-      throw new this.platform.api.hap.HapStatusError(HAPStatus.OPERATION_TIMED_OUT);
-    }
-    return targetTemperature;
   }
 
 }
