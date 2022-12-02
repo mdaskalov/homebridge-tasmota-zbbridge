d945c64c6159fefaba5e497eaf3da483a71259ff
Milko Daskalov <mdaskalov@outlook.com>
2022-12-02 16:35:57 +0100

    added action mapping

--- src/zigbee2MQTTMapping.ts
+++ src/zigbee2MQTTMapping.ts
@@ -45,6 +45,7 @@ export const EXPOSES = {
     },
   },
   // Generic exposes (by exposed.name)
+  action: { StatelessProgrammableSwitch: 'ProgrammableSwitchEvent' },
   battery: { Battery: 'BatteryLevel' },
   battery_low: { Battery: 'StatusLowBattery' },
   temperature: { TemperatureSensor: 'CurrentTemperature' },
@@ -57,4 +58,22 @@ export const EXPOSES = {
   carbon_monoxide: { CarbonMonoxideSensor: 'CarbonMonoxideDetected' },
   water_leak: { LeakSensor: 'LeakDetected' },
   gas: { LeakSensor: 'LeakDetected' },
+};
+
+export const ENUMS = {
+  action: {
+    toggle: 0,
+    arrow_left_click: 1,
+    arrow_left_hold: 1,
+    arrow_left_release: 1,
+    arrow_right_click: 2,
+    arrow_right_hold: 2,
+    arrow_right_release: 2,
+    brightness_down_click: 1,
+    brightness_down_hold: 1,
+    brightness_down_release: 1,
+    brightness_up_click: 2,
+    brightness_up_hold: 2,
+    brightness_up_release: 2,
+  },
 };
