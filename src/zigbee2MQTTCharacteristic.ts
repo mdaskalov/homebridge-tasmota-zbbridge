bd34f9618d9a476bbbb9f602c54694a0596e1430
Milko Daskalov <mdaskalov@outlook.com>
2022-12-02 19:06:23 +0100

    added value mapping

--- src/zigbee2MQTTCharacteristic.ts
+++ src/zigbee2MQTTCharacteristic.ts
@@ -9,6 +9,7 @@ import {
 
 import { TasmotaZbBridgePlatform } from './platform';
 import { Z2MExpose } from './zigbee2MQTTAcessory';
+import { ENUMS } from './zigbee2MQTTMapping';
 
 const UPDATE_TIMEOUT = 2000;
 
@@ -16,8 +17,10 @@ export class Zigbee2MQTTCharacteristic {
   public props: CharacteristicProps;
   public value: CharacteristicValue;
   private setValue: CharacteristicValue;
+  private getTs: number;
   private setTs: number;
-  private updateTs: number;
+  private awaitGetUpdate = false;
+  private awaitSetUpdate = false;
 
   public onGet?: () => CharacteristicValue | undefined;
   public onSet?: (value: CharacteristicValue) => void;
@@ -30,7 +33,7 @@ export class Zigbee2MQTTCharacteristic {
     readonly exposed: Z2MExpose,
   ) {
     this.setTs = Date.now() - UPDATE_TIMEOUT;
-    this.updateTs = Date.now();
+    this.getTs = Date.now() - UPDATE_TIMEOUT;
 
     const characteristic = this.service.getCharacteristic(this.platform.Characteristic[this.characteristicName]);
     if (characteristic !== undefined) {
@@ -39,12 +42,10 @@ export class Zigbee2MQTTCharacteristic {
       this.setValue = this.value;
       //this.log('characteristic props: %s', JSON.stringify(this.props));
       if (this.props.perms.includes(this.platform.api.hap.Perms.PAIRED_READ)) {
-        characteristic
-          .onGet(this.onGetValue.bind(this));
+        characteristic.onGet(this.onGetValue.bind(this));
       }
       if (this.props.perms.includes(this.platform.api.hap.Perms.PAIRED_WRITE)) {
-        characteristic
-          .onSet(this.onSetValue.bind(this));
+        characteristic.onSet(this.onSetValue.bind(this));
       }
     } else {
       throw (`Unable to initialize characteristic: ${this.characteristicName}`);
@@ -69,76 +70,170 @@ export class Zigbee2MQTTCharacteristic {
     return (Date.now() > (ts + UPDATE_TIMEOUT));
   }
 
-  private updateValue(value: CharacteristicValue): boolean {
-    const now = Date.now();
-    const oldValue = this.value;
-    const updateTs = ZbBridgeAccessory.formatTs(this.updateTs);
-    const setTs = ZbBridgeAccessory.formatTs(this.setTs);
-    let ignored = (value === oldValue) || ((this.setTs > this.updateTs) && !this.timeouted(this.setTs));
-    if (!ignored) {
-      this.value = value;
-      this.updateTs = now;
-    } else if (value === this.setValue) {
-      this.value = value;
-      this.updateTs = now;
-      ignored = true;
-    }
-    this.log('updateValue: %s, old: %s, set: %s, setTs: %s, updateTs: %s%s',
-      value,
-      oldValue,
-      this.setValue,
-      setTs,
-      updateTs,
-      (ignored ? ' (ignored)' : ''),
-    );
-    return ignored;
-  }
-
   private async onGetValue(): Promise<CharacteristicValue> {
-    const updated = (this.updateTs >= this.setTs);
-    const timeouted = this.timeouted(this.setTs);
-
-    const notUpdated = !updated && !timeouted;
-    const needsUpdate = !updated && timeouted;
-
-    let value: CharacteristicValue | undefined = notUpdated ? this.setValue : this.value;
-
-    if (needsUpdate && this.onGet !== undefined) {
-      value = this.onGet();
+    if (this.awaitGetUpdate && !this.timeouted(this.getTs)) {
+      return this.value;
     }
-    if (value === undefined) {
+    if (this.awaitSetUpdate && !this.timeouted(this.setTs)) {
       throw new this.platform.api.hap.HapStatusError(HAPStatus.OPERATION_TIMED_OUT);
     }
-    if (value !== this.value) {
+    if (this.onGet !== undefined) {
+      this.getTs = Date.now();
+      this.awaitGetUpdate = true;
+      const value = this.checkHBValue(this.onGet());
+      if (value === undefined) {
+        throw new this.platform.api.hap.HapStatusError(HAPStatus.OPERATION_TIMED_OUT);
+      }
       this.value = value;
     }
-    if (this.props.minValue && value < this.props.minValue) {
-      value = this.props.minValue;
+    return this.value;
+  }
+
+  private async onSetValue(value: CharacteristicValue) {
+    if (this.onSet !== undefined) {
+      const setValue = this.mapZ2MValue(value);
+      if (setValue !== undefined) {
+        this.onSet(setValue);
+        this.setValue = setValue;
+        this.setTs = Date.now();
+        this.awaitSetUpdate = true;
+        return;
+      }
     }
-    if (this.props.maxValue && value > this.props.maxValue) {
-      value = this.props.maxValue;
+    this.value = value;
+  }
+
+  update(value: CharacteristicValue | undefined) {
+    const updatedValue = this.mapHBValue(value);
+    if (updatedValue !== undefined) {
+      if (this.awaitGetUpdate) {
+        this.service.getCharacteristic(this.platform.Characteristic[this.characteristicName]).updateValue(updatedValue);
+        this.awaitGetUpdate = false;
+      }
+
+      if (this.awaitSetUpdate) {
+        if (value === this.setValue) {
+          this.value = updatedValue; // confirmed
+          this.awaitSetUpdate = false;
+          return;
+        }
+        if (!this.timeouted(this.setTs)) {
+          return; // ignore
+        }
+        this.awaitSetUpdate = false;
+      }
+      if (updatedValue !== this.value) {
+        //this.log('update: %s, old: %s', updatedValue, this.value);
+        this.service.getCharacteristic(this.platform.Characteristic[this.characteristicName]).updateValue(updatedValue);
+        this.value = updatedValue;
+      }
+    }
+  }
+
+  // homebridge -> Zigbee2MQTT
+  mapZ2MValue(value: CharacteristicValue): CharacteristicValue | undefined {
+    switch (this.exposed.type) {
+      case 'binary':
+        if (value as boolean === true) {
+          return this.exposed.value_on;
+        }
+        if (value as boolean === false) {
+          return this.exposed.value_off;
+        }
+        break;
+      case 'numeric':
+        return isNaN(value as number) ? undefined : this.mapZ2MNumericValue(value);
+    }
+  }
+
+  // Zigbee2MQTT -> homebridge
+  mapHBValue(value: CharacteristicValue | undefined): CharacteristicValue | undefined {
+    if (value !== undefined) {
+      switch (this.exposed.type) {
+        case 'binary':
+          if (value === this.exposed.value_on) {
+            return true;
+          }
+          if (value === this.exposed.value_off) {
+            return false;
+          }
+          return value;
+        case 'enum': {
+          const customindex = ENUMS[this.exposed.property][value];
+          const index = customindex !== undefined ? customindex : this.exposed.values.indexOf(value as string);
+          //this.log('got %s as index of %s:%s', index, this.exposed.property, value);
+          return (index === undefined ? 0 : index);
+        }
+        case 'numeric':
+          return isNaN(value as number) ? undefined : this.mapHBNumericValue(value);
+      }
+    }
+  }
+
+  private checkHBValue(value: CharacteristicValue | undefined): CharacteristicValue | undefined {
+    if (value === undefined) {
+      return value;
+    }
+    //this.log('return: %s :- min: %s, max: %s', value, this.props.minValue, this.props.maxValue);
+    if (this.props.minValue !== undefined && value as number < this.props.minValue) {
+      return this.props.minValue;
+    }
+    if (this.props.maxValue !== undefined && value as number > this.props.maxValue) {
+      return this.props.maxValue;
     }
     return value;
   }
 
-  private async onSetValue(value: CharacteristicValue) {
-    this.setValue = value;
-    this.setTs = Date.now();
-    if (this.onSet !== undefined) {
-      this.onSet(value);
+  private checkZ2MValue(value: CharacteristicValue | undefined): CharacteristicValue | undefined {
+    if (value === undefined) {
+      return value;
     }
+    //this.log('return: %s :- min: %s, max: %s', value, this.exposed.value_min, this.exposed.value_max);
+    if (this.exposed.value_min !== undefined && value as number < this.exposed.value_min) {
+      return this.exposed.value_min;
+    }
+    if (this.exposed.value_max !== undefined && value as number > this.exposed.value_max) {
+      return this.exposed.value_max;
+    }
+    return value;
   }
 
-  update(value: CharacteristicValue | undefined): string {
-    let statusText = '';
-    if (value !== undefined) {
-      const updateIgnored = this.updateValue(value);
-      if (!updateIgnored) {
-        this.service.getCharacteristic(this.platform.Characteristic[this.characteristicName]).updateValue(value);
-        statusText += ` ${this.characteristicName}: ${value}`;
-      }
+  // homebridge -> Zigbee2MQTT
+  mapZ2MNumericValue(value: CharacteristicValue): CharacteristicValue | undefined {
+    if (this.props.minValue === undefined || this.props.maxValue === undefined) {
+      return undefined;
     }
-    return statusText;
+    if (this.exposed.value_min === undefined || this.exposed.value_max === undefined) {
+      return undefined;
+    }
+    if (this.props.minValue === this.exposed.value_min && this.props.maxValue === this.exposed.value_max) {
+      return undefined;
+    }
+    const mappedValue = Zigbee2MQTTCharacteristic.mapValue(
+      value as number,
+      this.props.minValue, this.props.maxValue,
+      this.exposed.value_min, this.exposed.value_max,
+    );
+    return this.checkZ2MValue(mappedValue);
+  }
+
+  // Zigbee2MQTT -> homebridge
+  mapHBNumericValue(value: CharacteristicValue): CharacteristicValue | undefined {
+    if (this.exposed.value_min === undefined || this.exposed.value_max === undefined) {
+      return undefined;
+    }
+    if (this.props.minValue === undefined || this.props.maxValue === undefined) {
+      return undefined;
+    }
+    if (this.exposed.value_min === this.props.minValue && this.exposed.value_max === this.props.maxValue) {
+      return undefined;
+    }
+    const mappedValue = Zigbee2MQTTCharacteristic.mapValue(
+      value as number,
+      this.exposed.value_min, this.exposed.value_max,
+      this.props.minValue, this.props.maxValue,
+    );
+    return this.checkHBValue(mappedValue);
   }
 
   log(message: string, ...parameters: unknown[]): void {
