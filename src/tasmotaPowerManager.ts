5d120b5c12ae38ebf838493c6a0774afc155ce68
Milko Daskalov <mdaskalov@outlook.com>
2022-12-03 22:04:19 +0100

    fix assigment

--- src/tasmotaPowerManager.ts
+++ src/tasmotaPowerManager.ts
@@ -57,7 +57,7 @@ export class TasmotaPowerManager {
   }
 
   setState(ieee_address: string, state: boolean): boolean {
-    const accessory = this.accessories.find(a => a.ieee_address = ieee_address);
+    const accessory = this.accessories.find(a => a.ieee_address === ieee_address);
     if (accessory !== undefined) {
       this.mqttClient.publish('cmnd/' + accessory.topic, state ? 'ON' : 'OFF');
       accessory.state = state;
