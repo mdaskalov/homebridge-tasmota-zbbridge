4084c9d526b25fb8cd815502a167ee34ad3ffe3d
Milko Daskalov <mdaskalov@outlook.com>
2024-11-24 11:49:15 +0100

    added legacy definitions

--- src/tasmotaDeviceTypes.ts
+++ src/tasmotaDeviceTypes.ts
@@ -1,79 +1,54 @@
 
-import { TasmotaDeviceDefinition, TasmotaSensorDefinition } from './tasmotaAccessory';
-
-export const ACCESSORY_INFORMATION: TasmotaDeviceDefinition = {
-  AccessoryInformation: {
-    Manufacturer: {
-      get: {cmd: 'MODULE0', res: {path: 'Module.0'}},
-      stat: {update: false},
-      default: 'Tasmota',
-    },
-    Model: {
-      get: {cmd: 'DeviceName'},
-      stat: {update: false},
-      default: '{deviceName}',
-    },
-    SerialNumber: {
-      get: {cmd: 'STATUS 5', res: {topic: '{stat}/STATUS5', path: 'StatusNET.Mac'}},
-      stat: {update: false},
-      default: 'Unknown',
-    },
-    FirmwareRevision: {
-      get: {cmd: 'STATUS 2', res: {topic: '{stat}/STATUS2', path: 'StatusFWR.Version', mapping: {separator: '(', index: 0}}},
-      stat: {update: false},
-      default: 'Unknown',
-    },
-  },
-};
+import { TasmotaDeviceDefinition, TasmotaSensorDefinition } from './tasmotaDeviceDefinition';
 
 export const SENSOR_TYPES: TasmotaSensorDefinition[] = [
-  {key: 'Temperature', service: 'TemperatureSensor', characteristic: 'CurrentTemperature'},
-  {key: 'Humidity', service: 'HumiditySensor', characteristic: 'CurrentRelativeHumidity'},
+  { key: 'Temperature', service: 'TemperatureSensor', characteristic: 'CurrentTemperature' },
+  { key: 'Humidity', service: 'HumiditySensor', characteristic: 'CurrentRelativeHumidity' },
 ];
 
-export const DEVICE_TYPES: { [key: string] : TasmotaDeviceDefinition } = {
-  SWITCH: {Switch: {On: {get: {cmd: 'POWER{idx}'}}}},
-  LIGHT: {Lightbulb: {On: {get:{cmd: 'POWER{idx}'}}}},
-  LIGHT_B: {
+export const DEVICE_TYPES: { [key: string]: TasmotaDeviceDefinition } = {
+  SWITCH: { Switch: { On: { get: { cmd: 'POWER{idx}' } } } },
+  LIGHTBULB: { Lightbulb: { On: { get: { cmd: 'POWER{idx}' } } } },
+  LIGHTBULB_B: {
     Lightbulb: {
-      On: {get: {cmd: 'POWER{idx}'}},
-      Brightness: {get: {cmd: 'Dimmer'}},
+      On: { get: { cmd: 'POWER{idx}' } },
+      Brightness: { get: { cmd: 'Dimmer' } },
     },
   },
-  LIGHT_B_CT: {
+  LIGHTBULB_B_CT: {
     Lightbulb: {
-      On: {get: {cmd: 'POWER{idx}'}},
-      Brightness: {get: {cmd: 'Dimmer'}},
-      ColorTemperature: {get: {cmd: 'CT'}, props: {minValue: 153}},
+      On: { get: { cmd: 'POWER{idx}' } },
+      Brightness: { get: { cmd: 'Dimmer' } },
+      ColorTemperature: { get: { cmd: 'CT' }, props: { minValue: 153 } },
     },
   },
-  LIGHT_B_HS: {
+  LIGHTBULB_B_HS: {
     Lightbulb: {
-      On: {get: {cmd: 'POWER{idx}'}},
-      Brightness: {get: {cmd: 'Dimmer'}},
+      On: { get: { cmd: 'POWER{idx}' } },
+      Brightness: { get: { cmd: 'Dimmer' } },
       Hue: {
-        get: {cmd: 'HSBColor', res: {mapping: {index: 0}}},
-        set: {cmd: 'HSBColor1', res: {path: 'HSBColor'}},
+        get: { cmd: 'HSBColor', res: { mapping: { index: 0 } } },
+        set: { cmd: 'HSBColor1', res: { path: 'HSBColor' } },
       },
       Saturation: {
-        get: {cmd: 'HSBColor', res: {mapping: {index: 1}}},
-        set: {cmd: 'HSBColor2', res: {path: 'HSBColor'}},
+        get: { cmd: 'HSBColor', res: { mapping: { index: 1 } } },
+        set: { cmd: 'HSBColor2', res: { path: 'HSBColor' } },
       },
     },
   },
-  LIGHT_B_HS_CT: {
+  LIGHTBULB_B_HS_CT: {
     Lightbulb: {
-      On: {get: {cmd: 'POWER{idx}'}},
-      Brightness: {get: {cmd: 'Dimmer'}},
+      On: { get: { cmd: 'POWER{idx}' } },
+      Brightness: { get: { cmd: 'Dimmer' } },
       Hue: {
-        get: {cmd: 'HSBColor', res: {mapping: {index: 0}}},
-        set: {cmd: 'HSBColor1', res: {path: 'HSBColor'}},
+        get: { cmd: 'HSBColor', res: { mapping: { index: 0 } } },
+        set: { cmd: 'HSBColor1', res: { path: 'HSBColor' } },
       },
       Saturation: {
-        get: {cmd: 'HSBColor', res: {mapping: {index: 1}}},
-        set: {cmd: 'HSBColor2', res: {path: 'HSBColor'}},
+        get: { cmd: 'HSBColor', res: { mapping: { index: 1 } } },
+        set: { cmd: 'HSBColor2', res: { path: 'HSBColor' } },
       },
-      ColorTemperature: {get: {cmd: 'CT'}, props: {minValue: 153}},
+      ColorTemperature: { get: { cmd: 'CT' }, props: { minValue: 153 } },
     },
   },
   BUTTON: {
@@ -82,7 +57,7 @@ export const DEVICE_TYPES: { [key: string] : TasmotaDeviceDefinition } = {
         stat: {
           path: 'Button{idx}.Action',
           update: true,
-          mapping: [{from: 'SINGLE', to: 0}, {from: 'DOUBLE', to: 1}, {from: 'HOLD', to: 3}],
+          mapping: [{ from: 'SINGLE', to: 0 }, { from: 'DOUBLE', to: 1 }, { from: 'HOLD', to: 3 }],
         },
       },
     },
@@ -92,19 +67,19 @@ export const DEVICE_TYPES: { [key: string] : TasmotaDeviceDefinition } = {
       ContactSensorState: {
         get: {
           cmd: 'STATUS 10',
-          res: {topic: '{stat}/STATUS10', path: 'StatusSNS.Switch{idx}', mapping: [{from: 'ON', to: 0}, {from: 'OFF', to: 1}]},
+          res: { topic: '{stat}/STATUS10', path: 'StatusSNS.Switch{idx}', mapping: [{ from: 'ON', to: 0 }, { from: 'OFF', to: 1 }] },
         },
-        stat: {path: 'Switch{idx}.Action'},
+        stat: { path: 'Switch{idx}.Action' },
       },
     },
   },
   VALVE: {
     Valve: {
       Active: {
-        get: {cmd: 'POWER{idx}', res: {shared: true, mapping: [{from: 'ON', to: 1}, {from: 'OFF', to: 0}]}},
+        get: { cmd: 'POWER{idx}', res: { shared: true, mapping: [{ from: 'ON', to: 1 }, { from: 'OFF', to: 0 }] } },
       },
       InUse: {
-        stat: {path: 'POWER{idx}', mapping: [ {from: 'ON', to: 1}, {from: 'OFF', to: 0}]},
+        stat: { path: 'POWER{idx}', mapping: [{ from: 'ON', to: 1 }, { from: 'OFF', to: 0 }] },
       },
       ValveType: {
         default: 3,
@@ -117,10 +92,68 @@ export const DEVICE_TYPES: { [key: string] : TasmotaDeviceDefinition } = {
   LOCK: {
     LockMechanism: {
       LockTargetState: {
-        get: {cmd: 'POWER{idx}', res: {shared: true, mapping: [ {from: 'ON', to: 1}, {from: 'OFF', to: 0}]}},
+        get: { cmd: 'POWER{idx}', res: { shared: true, mapping: [{ from: 'ON', to: 1 }, { from: 'OFF', to: 0 }] } },
       },
       LockCurrentState: {
-        stat: {path: 'POWER{idx}', mapping: [ {from: 'ON', to: 1}, {from: 'OFF', to: 0}]},
+        stat: { path: 'POWER{idx}', mapping: [{ from: 'ON', to: 1 }, { from: 'OFF', to: 0 }] },
+      },
+    },
+  },
+
+  // backward compatibility
+  POWER: { Switch: { On: { get: { cmd: 'POWER' } } } },
+  POWER1: { Switch: { On: { get: { cmd: 'POWER1' } } } },
+  POWER2: { Switch: { On: { get: { cmd: 'POWER2' } } } },
+  POWER3: { Switch: { On: { get: { cmd: 'POWER3' } } } },
+  POWER4: { Switch: { On: { get: { cmd: 'POWER4' } } } },
+  LIGHT: { Lightbulb: { On: { get: { cmd: 'POWER' } } } },
+  LIGHT1: { Lightbulb: { On: { get: { cmd: 'POWER1' } } } },
+  LIGHT2: { Lightbulb: { On: { get: { cmd: 'POWER2' } } } },
+  LIGHT3: { Lightbulb: { On: { get: { cmd: 'POWER3' } } } },
+  LIGHT4: { Lightbulb: { On: { get: { cmd: 'POWER4' } } } },
+  HSBColor: {
+    Lightbulb: {
+      On: { get: { cmd: 'POWER{idx}' } },
+      Brightness: { get: { cmd: 'Dimmer' } },
+      Hue: {
+        get: { cmd: 'HSBColor', res: { mapping: { index: 0 } } },
+        set: { cmd: 'HSBColor1', res: { path: 'HSBColor' } },
+      },
+      Saturation: {
+        get: { cmd: 'HSBColor', res: { mapping: { index: 1 } } },
+        set: { cmd: 'HSBColor2', res: { path: 'HSBColor' } },
+      },
+    },
+  },
+  'StatusSNS.AM2301.Temperature': {
+    TemperatureSensor: {
+      CurrentTemperature: {
+        get: { cmd: 'STATUS 10', res: { topic: '{stat}/STATUS10', path: 'StatusSNS.AM2301.Temperature' } },
+        stat: { topic: '{sensor}', path: 'AM2301.Temperature' },
+      },
+    },
+  },
+  'StatusSNS.AM2301.Humidity': {
+    HumiditySensor: {
+      CurrentRelativeHumidity: {
+        get: { cmd: 'STATUS 10', res: { topic: '{stat}/STATUS10', path: 'StatusSNS.AM2301.Humidity' } },
+        stat: { topic: '{sensor}', path: 'AM2301.Humidity' },
+      },
+    },
+  },
+  'StatusSNS.DHT11.Temperature': {
+    TemperatureSensor: {
+      CurrentTemperature: {
+        get: { cmd: 'STATUS 10', res: { topic: '{stat}/STATUS10', path: 'StatusSNS.DHT11.Temperature' } },
+        stat: { topic: '{sensor}', path: 'DHT11.Temperature' },
+      },
+    },
+  },
+  'StatusSNS.DHT11.Humidity': {
+    HumiditySensor: {
+      CurrentRelativeHumidity: {
+        get: { cmd: 'STATUS 10', res: { topic: '{stat}/STATUS10', path: 'StatusSNS.DHT11.Humidity' } },
+        stat: { topic: '{sensor}', path: 'DHT11.Humidity' },
       },
     },
   },
